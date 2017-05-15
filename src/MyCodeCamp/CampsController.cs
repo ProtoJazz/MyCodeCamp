f737d7c3ab4db86420232bb771fc6a337e779fd6
Jim MacKenzie <protojazz@gmail.com>
2017-05-14 20:52:53 -0500

    First commit

---
+++ src/MyCodeCamp/CampsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyCodeCamp.Data;
+using MyCodeCamp.Data.Entities;
+
+namespace MyCodeCamp
+{
+    [Route("api/[controller]")]
+    public class CampsController : Controller
+    {
+        private ICampRepository _repo;
+
+        public CampsController(ICampRepository repo)
+        {
+            _repo = repo;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var camps = _repo.GetAllCamps();
+
+            return Ok(camps);
+        }
+        [HttpGet("{id}", Name = "CampGet")]
+        public IActionResult Get(int id, bool includeSpeakers = false)
+        {
+            try
+            {
+                Camp camp = null;
+                camp = includeSpeakers ? _repo.GetCampWithSpeakers(id) : _repo.GetCamp(id);
+                if (camp == null) return NotFound($"Camp {id} was not found");
+                return Ok(camp);
+            }
+            catch
+            {
+
+            }
+            return BadRequest();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Camp model)
+        {
+            try
+            {
+                _repo.Add(model);
+                if (await _repo.SaveAllAsync())
+                {
+                    var newUri = Url.Link("CampGet", new {id = model.Id});
+                    return Created(newUri, model);
+                }
+            }
+            catch (Exception e)
+            {
+               
+            }
+            return BadRequest();
+        }
+    }
+}
